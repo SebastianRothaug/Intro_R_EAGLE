8a3db614c488b7ca3e3ac45198e9d85c1fac47be
SebastianRothaug <sebastian-rothaug@gmx.de>
2024-10-29 20:14:56 +0100

    Initial

---
+++ Import2.R
@@ -0,0 +1,61 @@
+setwd("C:/Users/User/Desktop/R Course Intro to Programming/Intro_R_EAGLE/Data")
+
+df <- read.csv("Steigerwald_sample_points_all_data_subset_withNames.csv")
+
+head(df)
+summary(df)
+View(df)
+
+str(df) # giving structure just as int, num, character
+
+
+X <-  seq(1,100, by=2.5)
+X
+
+X[7]
+
+X[length(X)] #length giving you the entries (the volume) of the list at the same time its the last index
+X[length(X)-1] #second to last
+
+X[-1] #everything expect the first position
+
+idx <- c(1,2,6)
+X[idx] #giving only these defined positions
+X[-idx]
+
+
+X > 20
+(X<= 10) | (X>=20)
+
+
+X2 <- numeric(length(X))
+X2
+X2[X<30] <- 1 # X<30 refering to the values smaller than 30 cause of X in it 
+              # otherwise for replacing the first 30 position of the list X[1:30] <- 1
+X2
+
+#Matrix
+m1 <- matrix(c(4,7,3,8,9,2), nrow = 2)
+m1
+
+m1[,2] #2 colm
+
+
+
+numbers_1 <- rnorm(80,mean=0,sd=1)
+numbers_1
+
+mat_1 <- matrix(numbers_1,nrow=20,ncol=4)
+#Transform into a Matrix with 20 rows and 4 columns
+mat_1
+
+df_1 <- data.frame(mat_1)
+df_1
+
+#Data Frame
+
+df0 <- data.frame(A=c(1,2,3),B=c("aB1","aB2","aB3"))
+df0
+df0["A"]
+
+df0$B[3] #Abfrage mit Bedingung
---
+++ ImportTable.R
@@ -0,0 +1,41 @@
+setwd("C:/Users/User/Desktop/R Course Intro to Programming/Intro_R_EAGLE/Data")
+#set working directory to show the computer where to grab and store the data
+
+list.files() #listen up the files in the working directorty
+
+my.df <- read.csv("TestTable.csv",
+                  header = TRUE,
+                  sep = ";")
+#read for importing Table from your set working directory
+
+head(my.df)
+
+summary(my.df)
+
+plot(my.df)
+
+View(my.df)
+
+write.table(my.df, file = "my_data_frame.txt")
+
+
+c(NA, "Source\tTrain\tVal\tTest", "Latex\t187199\t7265\t5719", "Word\t73383\t2735\t2281", "Total\t260582\t10000\t8000")
+c("Name\" \"Location\" \"Number", "\"1\" \"Sebastian\" \"Germany\" 1", "\"2\" \"David\" \"France\" 44", "\"3\" \"Lutz\" \"Germany\" 2", "\"4\" \"Anna\" \"Italy\" 6", "\"5\" \"Jannis\" \"USA\" 7", "\"6\" \"Christian\" \"Denmark\" 3", "\"7\" \"Charl\" \"Africa\" 9")
+# datapasta Vector Ctrl + Shift + V
+
+data.frame(
+  stringsAsFactors = FALSE,
+            Source = c("Latex", "Word", "Total"),
+             Train = c(187199L, 73383L, 260582L),
+               Val = c(7265L, 2735L, 10000L),
+              Test = c(5719L, 2281L, 8000L)
+)
+# datapasta Dataframe Ctrl + Shift + F
+
+data.table::data.table(
+      Source = c("Latex", "Word", "Total"),
+       Train = c(187199L, 73383L, 260582L),
+         Val = c(7265L, 2735L, 10000L),
+        Test = c(5719L, 2281L, 8000L)
+)
+# datapasta Table Ctrl + Shift + T
