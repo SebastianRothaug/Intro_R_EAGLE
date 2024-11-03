1d8155ef56f40c005ca05089a8b0c78f7ae888cc
SebastianRothaug <sebastian-rothaug@gmx.de>
2024-11-03 10:39:38 +0100

    setwd = R_Course

--- Import2.R
+++ Import2.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/User/Desktop/R Course Intro to Programming/Intro_R_EAGLE/Data")
+setwd("C:/Users/User/Desktop/R_Course/Intro_R_EAGLE/Data")
 
 df <- read.csv("Steigerwald_sample_points_all_data_subset_withNames.csv")
 
